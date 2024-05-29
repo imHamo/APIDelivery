fe9bb5d25c3da4f63bb1f4d24691f50824c49e21
Mohamed Saber <79617976+imHamo@users.noreply.github.com>
2024-05-29 17:41:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,17 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+const API_KEY = '0b688e0bd491bd0c5193f1f9dabc482c35c396a1'; // Your Delhivery API Key
+
 app.get('/api/proxy', async (req, res) => {
   const { waybill } = req.query;
 
   try {
-    const response = await axios.get(`https://track.delhivery.com/api/v1/packages/json/?waybill=${waybill}`);
+    const response = await axios.get(`https://track.delhivery.com/api/v1/packages/json/?waybill=${waybill}`, {
+      headers: {
+        'Authorization': `Token ${API_KEY}`
+      }
+    });
     res.json(response.data);
   } catch (error) {
     res.status(500).json({ error: 'Error fetching data from Delhivery API' });
